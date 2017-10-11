949c6b59c5606b5b0dd2470a95e6ebc263c22053
John Desrosiers <johndrunner@yahoo.com>
2017-10-11 06:53:30 -0700

    server runs with minimal routing

--- server.js
+++ server.js
@@ -2,7 +2,47 @@
  * 'npm start' */
 
 // Dependencies
-const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('config');
+const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const scraper = require('./lib/scraper.js');
+
+// Globals
+const PORT = 8080;
+
+// Models
+const Article = require('./models/Article.js');
+// const Comment = require('./models/Comment.js'); // TODO: add Comment model
+
+// use es6 native promises with mongoose
+mongoose.Promise = Promise;
+
+// initialize express
+const app = express();
+
+// Initialize the database connection
+mongoose.connect(config.DbHost, { useMongoClient: true } );
+const db = mongoose.connection;
+db.on('error', error => console.log('Mongoose Error: ', error));
+
+// setup logging based on NODE_ENV. no logging if testing.
+const env = config.util.getEnv('NODE_ENV');
+if (env === 'dev') app.use(logger('dev'));
+if (env !== 'dev' && env !== 'test') app.use(logger('tiny'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static('public')); // TODO: add public dir and files
+
+/* Routes
+ * ==================================================================== */
+app.get('/', (req, res) => res.send('TODO: serve landing page'));
+
+// Route will scrape for articles and save them to the db
+app.get('/scrape', (req, res) => {
+  res.send('TODO: scrape the web and return a message on success');
+});
+
+// start the server
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
