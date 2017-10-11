ca44df3cf4fbc57cb028455e2789b7e7d5f2b3f7
John Desrosiers <johndrunner@yahoo.com>
2017-10-11 07:48:30 -0700

    added properties to ArticleSchema

--- models/Article.js
+++ models/Article.js
@@ -5,9 +5,27 @@
 // Dependencies
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-const ArticleSchema = new Schema({});
+// TODO: add validation
+const ArticleSchema = new Schema({
+  author: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  headline: { type: String, required: true },
+  pubDate: Date,
+  summary: { type: String, required: true },
+  tags: Array,
+
+  // article url must be unique to prevent duplication of
+  // the article in the database
+  url: { type: String, required: true, unique: true }
+});
+
+// Set the created at parameter on save
+ArticleSchema.pre('save', (next) => {
+  if (!this.createdAt) this.createdAt = new Date();
+  next();
+});
 
 // Export a mongoose model for the Article
 module.exports = mongoose.model('Article', ArticleSchema);
