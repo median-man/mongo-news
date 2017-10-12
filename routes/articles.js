2fdfa3ed0806817fdbe29cf3b868b6b5d2fd88cb
John Desrosiers <johndrunner@yahoo.com>
2017-10-11 20:57:19 -0700

    refactored saveArticle

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,14 @@
 const Article = require('../models/Article.js');
 
+// Returns a promise for the updated article after setting the saved property
+function setSaved(id, saved) {
+  return Article.findByIdAndUpdate(
+    id,
+    { saved },
+    { new: true } // return the updated document
+  );
+}
+
 // Returns all articles from db in an array
 function getArticles(req, res) {
   Article.find()
@@ -9,15 +18,9 @@ function getArticles(req, res) {
 
 // Sets the saved property to true for the article with matching id
 function saveArticle(req, res) {
-  Article.findByIdAndUpdate(
-    req.body.id,
-    { saved: true },
-    { new: true }, // return the updated document
-    (err, article) => {
-      if (err) return res.status(404).json(err);
-      return res.json(article);
-    }
-  );
+  setSaved(req.body.id, true)
+    .then(article => res.json(article))
+    .catch(err => res.status(404).json(err));
 }
 
 module.exports = { getArticles, saveArticle };
