0b59c0857f2a3bd0616d25befbbbea49b2c858be
John Desrosiers <johndrunner@yahoo.com>
2017-10-11 19:28:14 -0700

    added Comment.js. schema incomplete.

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+/* Comment model
+ *
+ * Representation of a comment created by a user associated with an article */
+
+// Dependencies
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+// TODO: add validation
+const CommentSchema = new Schema({
+  createdAt: { type: Date, default: Date.now }
+});
+
+// Set the created at parameter on save
+CommentSchema.pre('save', (next) => {
+  if (!this.createdAt) this.createdAt = new Date();
+  next();
+});
+
+// Export a mongoose model for the Article
+module.exports = mongoose.model('Comment', CommentSchema);
