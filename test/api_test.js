dd51df1e84411a8f3adb555c5f27d80b533dc074
John Desrosiers <johndrunner@yahoo.com>
2017-10-11 19:22:20 -0700

    created some api tests

---
+++ test/api_test.js
@@ -0,0 +1,50 @@
+// set env to test
+process.env.NODE_ENV = 'test';
+
+/* global describe beforeEach it */
+
+const mongoose = require('mongoose');
+const Article = require('../models/Article');
+
+// dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+// globals
+const should = chai.should();
+
+// implement chaiHttp
+chai.use(chaiHttp);
+
+// Test the api
+describe('API routes', () => {
+
+  // test the /scrape route
+  describe('/GET scrape', () => {
+    it('it sends "Scrape Complete" and status code 200', (done) => {
+      chai.request(server)
+        .get('/scrape')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.text.should.be.equal('Scrape Complete');
+          done();
+        });
+    });
+  });
+
+  // test /articles route. assume that there are some articles in the database already.
+  describe('/GET articles', () => {
+    it('it should GET all the articles', (done) => {
+      chai.request(server)
+        .get('/articles')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.at.least(1);
+          done();
+        });
+    });
+  });
+
+});
