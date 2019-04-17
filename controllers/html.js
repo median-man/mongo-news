22e2f73c0fe7b608b7256c9e487701baa2804483
John Desrosiers <john.desro@gmail.com>
2019-04-17 06:45:47 -0700

    Adds filtering for article summary

--- controllers/html.js
+++ controllers/html.js
@@ -8,34 +8,37 @@ function articleDisplayDate(date) {
 }
 
 function enrichArticle(article) {
-  const { pubDate } = article;
-  let { datePublished } = article;
-  let displayDate;
-  if (datePublished) {
-    displayDate = articleDisplayDate(datePublished);
-  } else {
-    displayDate = pubDate;
-    datePublished = pubDate;
-  }
-  return { ...article._doc, displayDate, datePublished };
+  const { datePublished } = article;
+  const displayDate = articleDisplayDate(datePublished);
+  return { ...article._doc, displayDate };
 }
+
+const defaultArticleQueryConditions = {
+  datePublished: { $exists: true },
+  summary: { $exists: true },
+
+  // ignore articles with summary containing fewer than 15 characters.
+  // Usually saved as "Read more...". The bad data is occasionally scraped\
+  // (generally caused by a change in the html structure)
+  $expr: { $gt: [{ $strLenCP: '$summary' }, 15] },
+};
 
 // Sends index page with articles from the database to the client
 function getRoot(req, res) {
-  Article.find()
-    .then((articles) => {
-      const hbsObject = {
-        articles: articles.map(enrichArticle),
-      };
-      return res.render('index', hbsObject);
-    })
+  Article.find(defaultArticleQueryConditions)
+    .sort({ datePublished: 'desc' })
+    .then(articles => articles.map(enrichArticle))
+    .then(articles => res.render('index', { articles }))
     .catch(() => res.status(404).send('page unavailable'));
 }
 
 // Sends index page with saved articles to client
 function getSaved(req, res) {
-  Article.find({ saved: true })
-    .then(articles => res.render('index', { articles: articles.map(enrichArticle), saved: true }))
+  const conditions = { ...defaultArticleQueryConditions, saved: true };
+  Article.find(conditions)
+    .sort({ datePublished: 'desc' })
+    .then(articles => articles.map(enrichArticle))
+    .then(articles => res.render('index', { articles, saved: true }))
     .catch(() => res.stats(404).send('page unavailable'));
 }
 
